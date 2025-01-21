f0e9aa560fba4e44c52f34b2942bcdc4bd50dc97
Richter667 <Uldor1@ya.ru>
2025-01-21 18:25:54 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,13 @@
+username = "Richter" # 1.
+title = "Beginning" # 2.
+content = "Beginning action" #3.
+status = "In progress " #4.
+created_date = "20-01-25 " #5.
+issue_date = "20-02-25" #6.
+
+print ("Имя пользователя:", username)
+print ("Заголовок заметки:", title)
+print ("Описание заметки:", content)
+print ("Статус заметки:", status)
+print ("Дата создания заметки:", created_date [0:5])
+print ("Дата истечения заметки:", issue_date [0:5])
