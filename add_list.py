efd2bba32e10642486da4482fb97539c783c860a
Richter667 <Uldor1@ya.ru>
2025-01-23 15:48:09 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,7 @@
+zagolovok_1 = input("Основные темы: ")
+zagolovok_2 = input("Персонажи: ")
+zagolovok_3 = input("Рекомендации для чтения: ")
+
+zametka_knigi = ["Основные темы: " + zagolovok_1, "Персонажи: " + zagolovok_2, "Рекомендации для чтения: " + zagolovok_3]
+
+print("\n".join (zametka_knigi))
