2cd5af6398c9ba79de6fbd577ecb6836126a4aaf
Richter667 <Uldor1@ya.ru>
2025-01-25 12:13:44 +0300

    add_list.py

--- add_list.py
+++ add_list.py
@@ -1,3 +1,24 @@
+username = "Richter" # 1.
+title = "Beginning" # 2.
+content = "Beginning action" #3.
+status = "In progress " #4.
+created_date = "20-01-25 " #5.
+issue_date = "20-02-25" #6.
+
+print ("Имя пользователя:", username)
+print ("Заголовок заметки:", title)
+print ("Описание заметки:", content)
+print ("Статус заметки:", status)
+print ("Дата создания заметки:", created_date [0:5])
+print ("Дата истечения заметки:", issue_date [0:5])
+
+username = input ("Введите имя пользователя: ")
+title = input("Введите заголовок заметки: ")
+content = input("Введите описание заметки: ")
+status = input("Ведите статус заметки: ")
+created_date = input("Введите дату создания заметки: ")
+issue_date = input("Введите дату истечения заметки: ")
+
 zagolovok_1 = input("Основные темы: ")
 zagolovok_2 = input("Персонажи: ")
 zagolovok_3 = input("Рекомендации для чтения: ")
